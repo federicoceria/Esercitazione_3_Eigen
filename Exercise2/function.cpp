4195bd4f76346ad3682468d6e6e73d2696b58fb7
federicoceria <s308599@studenti.polito.it>
2025-04-07 14:26:09 +0200

    new changes

--- Exercise2/function.cpp
+++ Exercise2/function.cpp
@@ -8,11 +8,19 @@ using namespace Eigen;
 // matrici e vettori sono passati per riferimento
 void Palu_matrix(const Matrix2d &A, const Vector2d &b, const Vector2d &c, Vector2d &sol, double &err){
 
-// siccome la matrice A è 2x2 invertibile (buon condizionamento) uso la decomposizione PartialPivLU per velocizzare
-sol = A.partialPivLu().solve(b);
+// controllo se la matrice è invertibile tramite il determinante
+if (A.determinant() != 0) {
+	
+	// siccome la matrice A è 2x2 invertibile (buon condizionamento) uso la decomposizione PartialPivLU per velocizzare
+	sol = A.partialPivLu().solve(b);
 
-// uso la norma euclidea per calcolare l'errore relativo
-err = ((c - sol).norm()) / (c.norm());
+	// uso la norma euclidea per calcolare l'errore relativo
+	err = ((c - sol).norm()) / (c.norm());
+} 
+else {
+	// uso fullPivLu per avere maggiore robustezza
+	sol = A.fullPivLu().solve(b);
+}
 }
 
 // funzione per risolvere il sistema lineare attraverso la decomposizione QR
